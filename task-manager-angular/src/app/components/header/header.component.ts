c534314068af7ae3b28f2841d3e542ab8b76cafd
Kunal-JockL <kunalsalvak@gmail.com>
2024-05-16 00:22:08 +0530

    Created array for tasks

--- task-manager-angular/src/app/components/header/header.component.ts
+++ task-manager-angular/src/app/components/header/header.component.ts
@@ -1,12 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { ButtonComponent} from '../button/button.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
+
 export class HeaderComponent {
   title = 'Task Tracker';
+  i = 0;
+
+  toogleAddTask(){
+    this.i = this.i + 1;
+    console.log(this.i);
+  }
 }
