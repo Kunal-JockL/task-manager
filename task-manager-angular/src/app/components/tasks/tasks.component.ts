be2eab6d2e5f674643f125cc479a3436262bcc27
Kunal-JockL <kunalsalvak@gmail.com>
2024-05-17 00:20:59 +0530

    done with setting up tsks and starting with services

--- task-manager-angular/src/app/components/tasks/tasks.component.ts
+++ task-manager-angular/src/app/components/tasks/tasks.component.ts
@@ -1,16 +1,34 @@
-import { Component } from '@angular/core';
-import {TASKS} from '../../mock-tasks';
-import {Task} from '../../Task';
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../../Task';
 import { CommonModule } from '@angular/common';
-
+import { TaskItemComponent } from '../task-item/task-item.component';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TaskItemComponent],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-tasks: Task[] = TASKS;
+
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService){}
+
+  ngOnInit(): void{
+    this.tasks = this.taskService.getTasks(); 
+  }
 }
+
+
+
+
+
+function tests() {
+  let tasks: Task[] = [];
+  console.log(tasks);
+}
+
+tests();
